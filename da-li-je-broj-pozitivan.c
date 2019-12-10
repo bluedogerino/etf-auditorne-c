c30c6e7d234d8163f146de788dec7e6d03a638f6
Griffin <bluedogerino@gmail.com>
2019-12-10 15:29:23 +0100

    Create da-li-je-broj-pozitivan.c

---
+++ da-li-je-broj-pozitivan.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    double broj;
+    printf("Unesite broj : ");
+    scanf("%lf", &broj);
+    if (broj>0)
+        printf("Broj je pozitivan!\n");
+    return 0;
+}	
